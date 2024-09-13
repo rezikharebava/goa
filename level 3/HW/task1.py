e704fc3e1fa643e794c511ec8aac76dcb2868268
rezikharebava <kharebavarezikoo@gmail.com>
2024-09-13 22:32:51 +0900

    3

---
+++ level 3/HW/task1.py
@@ -0,0 +1,17 @@
+score = 1
+print(score)
+health = 100
+print(health)
+attacking_points =15 
+defence_points = 10
+print(attacking_points + defence_points)
+math_score= 8
+english_score=7
+science_score= 9
+chemistry_score= 6 
+history_score= 10
+print(math_score + english_score +science_score + chemistry_score + history_score )
+name = "rezi"
+surname = "kharebava"
+age = 14
+print(name + surname + str(age))
