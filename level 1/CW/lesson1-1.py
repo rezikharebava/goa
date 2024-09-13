6994176cc596e4b660b3b0aa4396bf1f33664632
rezikharebava <kharebavarezikoo@gmail.com>
2024-09-13 21:01:54 +0900

    2

---
+++ level 1/CW/lesson1-1.py
@@ -0,0 +1,39 @@
+name = "rezi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"rezi" არის ცვლადისთვის მნიშვნელობა
+
+surname = "kharebava"
+
+# print(name)
+#პრინტ ფუნქცია გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "rezi" # ეს არის str (string) ტიპის ცვლადი
+age =14 # ეს არის int ( integer ) მთელი რიცხვი
+height = 184.5 # ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming =True  #True ან false 
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+siUgly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " +surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
+
+name = "rezi"
+surname = "kharebava"
+height = 175
+weight = 85
+print(name +" " + surname + " " + str(age) + " " + str(height) + " " + str(weight))
---
+++ level 3/HW/task01.py
@@ -0,0 +1,12 @@
+name= 'rezi'
+
+name = 'rezi'
+
+surname = 'kharebava'
+
+print(name)
+print(surname)
+
+player_speed = 1
+
+print(player_speed)
